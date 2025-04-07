9420e6dfedba6157acac795a7bbc0a9dae04d7e6
sanche-19 <ceed@mail.ru>
2025-04-07 18:25:02 +0300

    Урок 07.04.25
    начало python

---
+++ python/07_04_25/main.py
@@ -0,0 +1,15 @@
+#Калькулятор
+value1 = int(input("Введите первую переменную:"))
+value2 = int(input("Введите вторую переменную:"))
+print(f"{value1} + {value2}={value1 + value2}")#Сумма элементов
+print(value1 + value2)#Сумма элементов
+#string - тип данных для текста
+#int -тип данных для целочисленных чисел
+#float- тип данных с плавающей точкой
+#boot -  тип данных логический, True, False
+print(f"{value1} - {value2} = {value1 - value2}")
+print(f"{value1} * {value2} = {value1 * value2}")
+print(f"{value1} / {value2} = {value1 / value2}")
+print(f"{value1} // {value2} = {value1 // value2}")
+print(f"{value1} % {value2} = {value1 % value2}")
+print(f"{value1} ** {value2} = {value1 ** value2}")
